ee061f2701b2a4724a436a779b4d733cc1e29567
Caleb McCreary <clsmccreary@gmail.com>
2024-09-06 00:43:51 -0700

    Initial Commit

---
+++ GoldenCup Watch App/GoldenCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldenCupApp.swift
+//  GoldenCup Watch App
+//
+//  Created by Caleb McCreary on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GoldenCup_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
